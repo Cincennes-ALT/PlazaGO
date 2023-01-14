f95faa00332a9e4b6ecdadd745cd15efce553422
Vincent <93611221+KitsuneRanger@users.noreply.github.com>
2023-01-14 23:36:56 +0100

    Plaza.go most már vissza adja a jelenleg játszott szám adatait

--- Plaza.go
+++ Plaza.go
@@ -9,63 +9,64 @@ import (
 
 func main() {
 	fmt.Println("Wellcome to PlazaGO!")
-
-	CurrentSong()
-}
-
-// struct of the current number
-type SongNow 
-
-// gets the current song
-func CurrentSong() {
-
+	GetAPI("status", "")
 }
 
 // api sample
-func GetAPI(WhatType string, WhatValue string, ReturnType string) string {
-	url := "https://api.plaza.one/" + WhatType + "/" + WhatValue
+func GetAPI(WhatType string, WhatValue string) { // the link to get api
+	url := "https://api.plaza.one/" + WhatType + "/" + WhatValue // link puzzle
 	method := "GET"
 	client := &http.Client{}
 
+	// send the request
 	request, err := http.NewRequest(method, url, nil)
-
 	if err != nil {
 		fmt.Println(err)
-		var hiba error = err
-		return (hiba.Error())
+		return
 	}
+
+	//get the request
 	response, err := client.Do(request)
 	if err != nil {
 		fmt.Println(err)
-		var hiba error = err
-		return (hiba.Error())
+		return
 	}
 
+	// body of the request
 	defer response.Body.Close()
-
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		fmt.Println(err)
-		var hiba error = err
-		return (hiba.Error())
+		return
 	}
 
-	// fmt.Println(string(body))
-	switch ReturnType {
-	case "string":
-		vissza := string(body)
-		return vissza
-	case "array":
-		var vissza [1]string
-		vissza[0] = string(body)
-		return vissza[0]
-	case "json":
-		vissza, err := json.Marshal(body)
-		if err != nil {
-			fmt.Println(err)
-		}
-		return string(vissza)
-	default:
-		return string(body)
+	// switch case based on the link
+	switch WhatType {
+	case "status":
+		var prepare map[string]map[string]string // its a map in a map sor the "song" key
+		json.Unmarshal(body, &prepare)
+		var fwdSong map[string]string = prepare["song"]
+		currentSong(fwdSong)
 	}
+
+}
+
+func currentSong(gotSatus map[string]string) {
+	// i kinda made that too complex
+	var songMeta SongNow = SongNow{fmt.Sprint(gotSatus["title"]), fmt.Sprint(gotSatus["artist"]), fmt.Sprint(gotSatus["album"]), fmt.Sprint(gotSatus["artwork_src"])}
+
+	title := songMeta.title
+	artist := songMeta.artist
+	album := songMeta.album
+	// albumCover := songMeta.albumCover
+
+	fmt.Printf("Currently playing: %s by %s from %s", title, artist, album)
+}
+
+/*
+	structs
+*/
+// Now playing sungs structs
+type SongNow struct {
+	artist, title, album, albumCover string
 }
